f12abf2e803d15ada0e261ed0ee20f3ddd045ee8
Chris Holdgraf <choldgraf@berkeley.edu>
2020-03-03 09:12:06 -0800

    using document and adding a more helpful error

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -46,9 +46,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 #
 html_theme = "pandas_sphinx_theme"
 html_logo = "_static/logo.png"
-html_theme_options = {
-    "github_url": "https://github.com/ExecutableBookProject/myst-nb",
-}
+html_theme_options = {"github_url": "https://github.com/ExecutableBookProject/myst-nb"}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
