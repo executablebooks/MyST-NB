6276e908e074794dee70fbffc2e2cf9bd8f2d578
Phil Austin <paustin@eos.ubc.ca>
2020-03-21 14:03:57 -0700

    to_push

---
+++ docs/examples/austin_issue/build_website.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -v
+#
+# move the ipynb files into the doc_notebooks folder
+#
+mkdir -p _build
+#
+# build the website
+#
+sphinx-build -N -v -b html . _build
+
+
--- docs/examples/austin_issue/conf.py
+++ docs/examples/austin_issue/conf.py
@@ -1,21 +1,5 @@
 # -*- coding: utf-8 -*-
 #
-# Configuration file for the Sphinx documentation builder.
-#
-# This file does only contain a selection of the most common options. For a
-# full list see the documentation:
-# http://www.sphinx-doc.org/en/stable/config
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
 # -- Project information -----------------------------------------------------
 
 project = "Numeric course"
@@ -42,10 +26,10 @@ extensions = [
     "sphinx.ext.mathjax",
 ]
 
-# extensions = [
-#     "myst_nb",
-#     "sphinx.ext.mathjax",
-# ]
+extensions = [
+    "myst_nb",
+    "sphinx.ext.mathjax",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -70,8 +54,6 @@ language = None
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
 exclude_patterns = [
-    "jupybook",
-    "Book",
     "_build",
     "Thumbs.db",
     ".DS_Store",
@@ -81,7 +63,6 @@ exclude_patterns = [
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = "sphinx"
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
@@ -89,103 +70,8 @@ pygments_style = "sphinx"
 #
 html_theme = "pydata_sphinx_theme"
 html_style = None
-html_theme_options = {"collapse_navigation": True}
-using_rtd_theme = True
-
-
-# nbsphinx_custom_formats = {
-#     ".py": lambda s: jupytext.reads(s, ".py"),
-# }
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
-# html_theme_options = {}
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-# Custom sidebar templates, must be a dictionary that maps document names
-# to template names.
-#
-# The default sidebars (for documents that don't match any pattern) are
-# defined by theme itself.  Builtin themes are using these templates by
-# default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
-# 'searchbox.html']``.
-#
-# html_sidebars = {}
-
-# -- Options for HTMLHelp output ---------------------------------------------
-
-# Output file base name for HTML help builder.
-htmlhelp_basename = "ComputationalStatisticsandStatisticalComputingdoc"
-
-
-# -- Options for LaTeX output ------------------------------------------------
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-     #
-    # 'papersize': 'letterpaper',
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (
-        master_doc,
-        "ComputationalStatisticsandStatisticalComputing.tex",
-        "Computational Statistics and Statistical Computing Documentation",
-        "Cliburn Chan",
-        "manual",
-    ),
-]
-
-
-# -- Options for manual page output ------------------------------------------
-
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [
-    (
-        master_doc,
-        "computationalstatisticsandstatisticalcomputing",
-        "Computational Statistics and Statistical Computing Documentation",
-        [author],
-        1,
-    )
-]
-
-
-# -- Options for Texinfo output ----------------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (
-        master_doc,
-        "ComputationalStatisticsandStatisticalComputing",
-        "Computational Statistics and Statistical Computing Documentation",
-        author,
-        "ComputationalStatisticsandStatisticalComputing",
-        "One line description of project.",
-        "Miscellaneous",
-    ),
-]
 
 
 # -- Extension configuration -------------------------------------------------
