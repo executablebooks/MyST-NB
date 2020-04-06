87dbb98c2e1683000e78865f7d2a00249efdf5fb
Chris Sewell <chrisj_sewell@hotmail.com>
2020-04-06 18:52:30 +0100

    update

--- setup.py
+++ setup.py
@@ -56,9 +56,9 @@ setup(
     extras_require={
         "code_style": ["flake8<3.8.0,>=3.7.0", "black", "pre-commit==1.17.0"],
         "testing": [
-            "coverage",
-            "pytest>=3.6,<4",
-            "pytest-cov",
+            "pytest~=5.3",
+            "pytest-cov~=2.8",
+            "coverage<5.0",
             "pytest-regressions",
             "beautifulsoup4",
             "matplotlib",
