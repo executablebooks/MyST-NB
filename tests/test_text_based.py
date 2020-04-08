c75beb182ad7b75559d871d5a3e20342b4b1842c
Chris Holdgraf <choldgraf@berkeley.edu>
2020-04-08 16:08:12 -0700

    updating tests for nested metadata

--- tests/test_text_based.py
+++ tests/test_text_based.py
@@ -9,6 +9,17 @@ def test_basic_run(sphinx_run, file_regression, check_nbs):
     sphinx_run.build()
     # print(sphinx_run.status())
     assert sphinx_run.warnings() == ""
-    assert sphinx_run.app.env.metadata == {"basic_unrun": {"author": "Chris"}}
+    assert set(sphinx_run.app.env.metadata["basic_unrun"].keys()) == {
+        "jupytext",
+        "kernelspec",
+        "author",
+        "source_map",
+        "language_info",
+    }
+    assert sphinx_run.app.env.metadata["basic_unrun"]["author"] == "Chris"
+    assert (
+        sphinx_run.app.env.metadata["basic_unrun"]["kernelspec"]
+        == '{"display_name": "Python 3", "language": "python", "name": "python3"}'
+    )
     file_regression.check(sphinx_run.get_nb(), check_fn=check_nbs, extension=".ipynb")
     file_regression.check(sphinx_run.get_doctree().pformat(), extension=".xml")
