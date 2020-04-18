71d7c4571cc3c74f37134e1afc02a46fa561d8f1
Phil Austin <paustin@eos.ubc.ca>
2020-04-18 10:05:38 -0700

    do img tags work

---
+++ docs/examples/image_test/build_website.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -v
+#
+# move the ipynb files into the doc_notebooks folder
+#
+mkdir -p _build
+#
+# build the website
+#
+sphinx-build -N -v -b html . _build/html
+rsync -avz media _build/html/
+mv _build/html/* _build/.
+
+
+
+
---
+++ docs/examples/image_test/index.rst
@@ -0,0 +1,8 @@
+Eqno test
+---------
+
+.. toctree::
+   :maxdepth: 1
+   
+   Test img tag <quiz2_2019t2.ipynb>
+
