937a184ccc2e18b2ff8038dacd2c4b3bb48db561
Chris Sewell <chrisj_sewell@hotmail.com>
2020-04-22 03:35:31 +0100

    pin pydata-sphinx-theme to exact version

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
             "plotly",
             "matplotlib",
             "sphinx-copybutton",
-            "pydata-sphinx-theme~=0.1",
+            "pydata-sphinx-theme==0.1.1",
         ],
     },
     zip_safe=True,
