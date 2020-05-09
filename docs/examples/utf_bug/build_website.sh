163e154c088f1126c84329637eebd8ae072ab290
Phil Austin <paustin@eoas.ubc.ca>
2020-05-09 10:40:01 -0700

    working for windows

--- docs/examples/utf_bug/build_website.sh
+++ docs/examples/utf_bug/build_website.sh
@@ -1,15 +1,5 @@
-#!/bin/bash -v
-#
-#
-mkdir -p ../_build/pyman
-rm -rf ../_build/pyman/*
-#
-# build the website
+
 #
 sphinx-build  -N -v -b html . _build/html
-rsync -avz _build/html/* ../_build/pyman/.
-
-
-
 
 
