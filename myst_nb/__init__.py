399e3e05fd3a2b9ad377b2f782b677434c5540f9
Chris Holdgraf <choldgraf@berkeley.edu>
2020-06-09 17:40:18 -0700

    adding code cell directive for warning

--- myst_nb/__init__.py
+++ myst_nb/__init__.py
@@ -3,6 +3,7 @@ __version__ = "0.8.1"
 from pathlib import Path
 
 from docutils import nodes
+from sphinx.util.docutils import SphinxDirective
 from sphinx.util import logging
 
 from jupyter_sphinx.ast import (  # noqa: F401
@@ -78,6 +79,26 @@ def update_togglebutton_classes(app, config):
 
 def save_glue_cache(app, env):
     NbGlueDomain.from_env(env).write_cache()
+
+
+class CodeCell(SphinxDirective):
+    """Raises a warning if it is triggered, it should not make it to the doctree."""
+
+    optional_arguments = 1
+    final_argument_whitespace = True
+    has_content = True
+
+    def run(self):
+        LOGGER.warning(
+            (
+                "Found a `code-cell` directive. To execute the `code-cell`, you must "
+                "add Jupytext header content to the page. See "
+                "https://myst-nb.readthedocs.io/en/latest/use/markdown.html "
+                "for more information."
+            ),
+            location=(self.env.docname, self.lineno),
+        )
+        return []
 
 
 def setup(app):
@@ -160,6 +181,7 @@ def setup(app):
     app.add_js_file("mystnb.js")
     app.setup_extension("jupyter_sphinx")
     app.add_domain(NbGlueDomain)
+    app.add_directive("code-cell", CodeCell)
 
     # TODO need to deal with key clashes in NbGlueDomain.merge_domaindata
     # before this is parallel_read_safe
--- tests/test_text_based.py
+++ tests/test_text_based.py
@@ -38,3 +38,14 @@ def test_basic_run_exec_off(sphinx_run, file_regression, check_nbs):
 
     file_regression.check(sphinx_run.get_nb(), check_fn=check_nbs, extension=".ipynb")
     file_regression.check(sphinx_run.get_doctree().pformat(), extension=".xml")
+
+
+@pytest.mark.sphinx_params(
+    "basic_nometadata.md",
+    conf={"jupyter_execute_notebooks": "off", "source_suffix": {".md": "myst-nb"}},
+)
+def test_basic_nometadata(sphinx_run, file_regression, check_nbs):
+    """A myst-markdown notebook with no jupytext metadata should raise a warning."""
+    sphinx_run.build()
+    # print(sphinx_run.status())
+    assert "Found a `code-cell` directive." in sphinx_run.warnings()
