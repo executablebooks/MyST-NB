e548060774655c757517a4c339f15eae7ec825f6
Chris Holdgraf <choldgraf@berkeley.edu>
2021-06-15 20:48:26 -0700

    Updating logo

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,8 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 #
 html_title = ""
 html_theme = "sphinx_book_theme"
-html_logo = "_static/logo.png"
+html_logo = "_static/logo-wide.svg"
+html_favicon = "_static/logo-square.svg"
 html_theme_options = {
     "github_url": "https://github.com/executablebooks/myst-nb",
     "repository_url": "https://github.com/executablebooks/myst-nb",
