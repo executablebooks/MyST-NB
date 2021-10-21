4fdba2afc4e4ef0bc8112b0b1915c6ae615f09a3
AakashGC <aakashgfude@gmail.com>
2021-10-22 01:31:43 +1100

    abspath instead

--- myst_nb/render_outputs.py
+++ myst_nb/render_outputs.py
@@ -472,8 +472,7 @@ class CellOutputRenderer(CellOutputRendererBase):
             filename = os.path.basename(output.metadata["filenames"][mime_type])
             # checks if file dir path is inside a subdir of dir
             filedir = os.path.dirname(output.metadata["filenames"][mime_type])
-            print(self.sphinx_dir, "--", filedir)
-            outbasedir = os.path.basename(self.sphinx_dir)
+            outbasedir = os.path.abspath(self.sphinx_dir)
             subpaths = filedir.split(outbasedir)
             final_dir = self.sphinx_dir
             if subpaths and len(subpaths) > 1:
