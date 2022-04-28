0e2c6d60a8cbaf95536d23b4245be8147ec8e92c
Chris Sewell <chrisj_sewell@hotmail.com>
2022-04-28 11:19:35 +0200

    🔧 MAINTAIN: Split sphinx extension into separate module

--- myst_nb/__init__.py
+++ myst_nb/__init__.py
@@ -5,7 +5,7 @@ __version__ = "0.14.0"
 def setup(app):
     """Sphinx extension setup."""
     # we import this locally, so sphinx is not automatically imported
-    from .sphinx_ import sphinx_setup
+    from .sphinx_ext import sphinx_setup
 
     return sphinx_setup(app)
 
