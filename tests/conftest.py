23bac883105bec37b4d7779d61feb949a4a77e33
Angus Hollands <goosey15@gmail.com>
2023-10-29 20:09:18 +0000

    refactor: guard regression behind version

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,6 +14,7 @@ from nbdime.diffing.notebooks import (
 )
 from nbdime.prettyprint import pretty_print_diff
 import nbformat as nbf
+import packaging.version
 import pytest
 import sphinx
 from sphinx import version_info as sphinx_version_info
@@ -264,28 +265,28 @@ def clean_doctree():
     return _func
 
 
-# comparison files will need updating
-# alternatively the resolution of https://github.com/ESSS/pytest-regressions/issues/32
-@pytest.fixture()
-def file_regression(file_regression):
-    return FileRegression(file_regression)
+if packaging.version.Version(sphinx.__version__) < packaging.version.Version("7"):
+    # comparison files will need updating
+    # alternatively the resolution of https://github.com/ESSS/pytest-regressions/issues/32
+    @pytest.fixture()
+    def file_regression(file_regression):
+        return FileRegression(file_regression)
 
+    class FileRegression:
+        ignores = (
+            # TODO: Remove when support for Sphinx<=6 is dropped,
+            re.escape(" translation_progress=\"{'total': 0, 'translated': 0}\""),
+            # TODO: Remove when support for Sphinx<7.2 is dropped,
+            r"original_uri=\"[^\"]*\"\s",
+        )
 
-class FileRegression:
-    ignores = (
-        # TODO: Remove when support for Sphinx<=6 is dropped,
-        re.escape(" translation_progress=\"{'total': 0, 'translated': 0}\""),
-        # TODO: Remove when support for Sphinx<7.2 is dropped,
-        r"original_uri=\"[^\"]*\"\s",
-    )
+        def __init__(self, file_regression):
+            self.file_regression = file_regression
 
-    def __init__(self, file_regression):
-        self.file_regression = file_regression
+        def check(self, data, **kwargs):
+            return self.file_regression.check(self._strip_ignores(data), **kwargs)
 
-    def check(self, data, **kwargs):
-        return self.file_regression.check(self._strip_ignores(data), **kwargs)
-
-    def _strip_ignores(self, data):
-        for ig in self.ignores:
-            data = re.sub(ig, "", data)
-        return data
+        def _strip_ignores(self, data):
+            for ig in self.ignores:
+                data = re.sub(ig, "", data)
+            return data
